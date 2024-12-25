0c323bd4af6e46adc44d52c88f4a30ef3c9eb766
raxeraditya <aditya1596k@gmail.com>
2024-12-25 09:09:58 +0530

    Stop tracking dist folder

--- Server/dist/lib/CookeiOptions.js
+++
@@ -1,6 +0,0 @@
-const cookieOPtionData = {
-    maxAge: 1 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    secure: false,
-};
-export default cookieOPtionData;
--- Server/dist/utils/getTokenData.js
+++
@@ -1,5 +0,0 @@
-const getTokenData = (req) => {
-    const token = req.cookies.jwt;
-    console.log("getToken", token);
-};
-export default getTokenData;
